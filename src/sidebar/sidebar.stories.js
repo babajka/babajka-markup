8f77d8e23d055f5ded73ab429e2b778bb851cac4
tyndria <tyndria23@gmail.com>
2019-01-18 21:22:04 +0300

    fix(main): add more sections, copy variables, cleaning up

--- src/sidebar/sidebar.stories.js
+++ src/sidebar/sidebar.stories.js
@@ -17,6 +17,24 @@ const TOPICS = [
   'філасофія',
 ];
 
+const PEOPLE = [
+  'Янка Купала',
+  'Уладзімір Караткевіч',
+  'Адам Міцкевіч',
+  'Якуб Колас',
+  'Ян Баршчэўскі',
+];
+
+const TIMES = ['1956 год', 'сення', 'у будучыні', '1863 год', 'XX стагоддзе'];
+
+/*
+TODO: 
+1. local problem with Ubuntu font
+2. Create basic structure of variables/ fonts for src
+3. what is the color of sidebar background?
+4. divider width
+*/
+
 const tile = storiesOf('src/Sidebar', module);
 tile.add('playground', ({ parameters: { defaultData } }) => {
   const russianLang = text('russian lang text', 'по-русски');
@@ -25,11 +43,21 @@ tile.add('playground', ({ parameters: { defaultData } }) => {
   const topicsTitle = text('topics title', 'Тэмы');
   const topics = array('topics', TOPICS, ',');
 
+  const peopleTitle = text('people title', 'асноўныя персаналіі');
+  const people = array('people', PEOPLE, ',');
+
+  const timeTitle = text('time title', 'асноўныя часы');
+  const times = array('times', TIMES, ',');
+
   const data = {
     ...defaultData,
     langs: [russianLang, englishLang],
     topicsTitle,
     topics,
+    peopleTitle,
+    people,
+    timeTitle,
+    times,
   };
 
   return `<div style="width: 500px">${render(data)}</div>`;
