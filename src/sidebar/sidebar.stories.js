d54d68764c1fd5313bff78918d0ebc8b7e5fdf15
tyndria <tyndria23@gmail.com>
2019-01-19 19:35:15 +0300

    feat(main): fixing bem(?), move data to separate file

--- src/sidebar/sidebar.stories.js
+++ src/sidebar/sidebar.stories.js
@@ -5,31 +5,29 @@ import render from './sidebar.ejs';
 import './sidebar.scss';
 import '../input/input.scss';
 
-import { TOPICS, PEOPLE, TIMES } from './staticData';
+import { TOPICS, PEOPLE, TIMES, LOCATIONS, PARTNERS, AUTHORS } from './staticData';
 
 const tile = storiesOf('src/Sidebar', module);
 tile.add('playground', ({ parameters: { defaultData } }) => {
   const russianLang = text('russian lang text', 'по-русски');
   const englishLang = text('english lang text', 'in english');
 
-  const topicsTitle = text('topics title', 'Тэмы');
-  const topics = array('topics', TOPICS, ',');
-
-  const peopleTitle = text('people title', 'асноўныя персаналіі');
-  const people = array('people', PEOPLE, ',');
-
-  const timeTitle = text('time title', 'асноўныя часы');
-  const times = array('times', TIMES, ',');
+  const topics = array('topics', TOPICS);
+  const people = array('people', PEOPLE);
+  const times = array('times', TIMES);
+  const locations = array('locations', LOCATIONS);
+  const partners = array('partners', PARTNERS);
+  const authors = array('authors', AUTHORS);
 
   const data = {
     ...defaultData,
     langs: [russianLang, englishLang],
-    topicsTitle,
     topics,
-    peopleTitle,
     people,
-    timeTitle,
     times,
+    locations,
+    partners,
+    authors,
   };
 
   return `<div style="width: 500px">${render(data)}</div>`;
