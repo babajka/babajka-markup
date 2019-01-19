5d5a64dc98cf354c712bf22ee1ca2617cbc1df6b
tyndria <tyndria23@gmail.com>
2019-01-19 18:22:32 +0300

    feat(main):  sidebar-add proper input, cleaning up

--- src/sidebar/sidebar.stories.js
+++ src/sidebar/sidebar.stories.js
@@ -3,37 +3,9 @@ import { text, array } from '@storybook/addon-knobs';
 
 import render from './sidebar.ejs';
 import './sidebar.scss';
+import '../input/input.scss';
 
-const TOPICS = [
-  'архітэктура',
-  'гісторыя',
-  'кіно',
-  'лінгвістыка',
-  'літаратура',
-  'мастацтва',
-  'міфалогія',
-  'музыка',
-  'тэатр',
-  'філасофія',
-];
-
-const PEOPLE = [
-  'Янка Купала',
-  'Уладзімір Караткевіч',
-  'Адам Міцкевіч',
-  'Якуб Колас',
-  'Ян Баршчэўскі',
-];
-
-const TIMES = ['1956 год', 'сення', 'у будучыні', '1863 год', 'XX стагоддзе'];
-
-/*
-TODO: 
-1. local problem with Ubuntu font
-2. Create basic structure of variables/ fonts for src
-3. what is the color of sidebar background?
-4. divider width
-*/
+import { TOPICS, PEOPLE, TIMES } from './staticData';
 
 const tile = storiesOf('src/Sidebar', module);
 tile.add('playground', ({ parameters: { defaultData } }) => {
