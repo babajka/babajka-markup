e49afb8d8109ff898900257a99a5a4c5e2d42f46
tyndria <tyndria23@gmail.com>
2019-02-13 22:49:35 +0300

    task(common): add dark theme

--- src/kit/kit.stories.js
+++ src/kit/kit.stories.js
@@ -4,6 +4,7 @@ import { text, boolean } from '@storybook/addon-knobs';
 import renderInput from './input/input.ejs';
 import renderLink from './link/link.ejs';
 
+import './stories.scss';
 import './input/input.scss';
 import './link/link.scss';
 
@@ -22,20 +23,22 @@ kit.add('Input', ({ parameters: { defaultData } }) => {
     loading,
   };
 
-  return `<div style="width: 250px; margin: 30px">${renderInput(data)}</div>`;
+  return `<div class="container">${renderInput(data)}</div>`;
 });
 
 kit.add('Link', ({ parameters: { defaultData } }) => {
   const disabled = boolean('is disabled', false);
   const linkText = text('text', 'wir.team@gmail.com');
   const rightIcon = text('right icon', 'user');
+  const isDarkTheme = boolean('is dark theme', false);
 
   const data = {
     ...defaultData,
     disabled,
     text: linkText,
     rightIcon,
+    isDarkTheme,
   };
 
-  return `<div style="width: 250px; margin: 30px">${renderLink(data)}</div>`;
+  return `<div class="container link ${isDarkTheme && 'theme-dark'}">${renderLink(data)}</div>`;
 });
